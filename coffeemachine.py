b46dd14838337570832f5481811be867199ade33
Isah Jacob <Isahjacob0@gmail.com>
2022-10-27 23:22:47 +0100

    remove the space from here

--- coffeemachine.py
+++ coffeemachine.py
@@ -73,7 +73,7 @@ def make_coffee(drink_name, order_ingredients):
     """ Deduct the required ingredients from the resources."""
     for item in order_ingredients:
         resources[item] -= order_ingredients[item]
-        print(f"Here is your {drink_name} ") 
+        print(f"Here is your {drink_name}") 
 
 
 is_on = True
