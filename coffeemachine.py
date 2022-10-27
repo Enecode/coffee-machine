df22e1d6dea19bb5f29329b720564e52def20d8a
Isah Jacob <Isahjacob0@gmail.com>
2022-10-27 17:48:50 +0100

    created a dictionary with three different types of drinks

---
+++ coffeemachine.py
@@ -0,0 +1,27 @@
+MENU = {
+    "tea": {
+        "ingredients": {
+            "water": 50,
+            "milk": 18,
+            "chocolate": 20,
+            "sugar": 10,
+        },
+        "cost": 4.5,
+    },
+
+    "espresso": {
+        "ingredients": {
+            "water": 50,
+            "coffee": 20,
+        },
+        "cost": 5.5
+    },
+    "conflates": {
+        "ingredients": {
+            "water": 50,
+            "sugar": 10,
+            "milk": 30,
+        },
+        "cost": 4.5,
+    },
+}
